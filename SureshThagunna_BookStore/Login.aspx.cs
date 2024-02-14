d293bccaf0a13460a1fae24c76675b6a95fdf7ee
Suresh <suresh.thagunna77@gmail.com>
2024-02-14 10:18:31 -0500

    add last minute comments

--- SureshThagunna_BookStore/Login.aspx.cs
+++ SureshThagunna_BookStore/Login.aspx.cs
@@ -12,12 +12,14 @@ namespace SureshThagunna_BookStore
 
         protected void loginButton_Click(object sender, EventArgs e)
         {
+            // store the first name in a session variable
             Session["FirstName"] = FirstName.Text;
             Response.Redirect("products");
         }
 
         protected void cancelButton_Click(object sender, EventArgs e)
         {
+            // redirect the user to the default page
             Response.Redirect("default");
         }
 
